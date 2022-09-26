0c0535d27117cec30d70a0b4e32cb47152ffb8cb
kai-5908 <dolce.cantabile.maestoso@gmail.com>
2022-09-26 22:53:51 +0900

    initial commit

---
+++ apps/minimalapp/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, Flaskbook!"
+
+
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello, {name}"
